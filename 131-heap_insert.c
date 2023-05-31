8b05ab6c222b6648363192406190f0b85a18b0a0
Abel Adisu <abeladisu73@gmail.com>
2023-05-31 00:30:14 -0700

    updated

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,142 +1,103 @@
 #include "binary_trees.h"
+size_t bts(const binary_tree_t *tree);
+heap_t *heap_nf(heap_t *root, size_t number);
+heap_t *heap_r(heap_t *new, heap_t *parent);
 
 /**
- * binary_tree_height - Function that measures the height of a binary tree
- * @tree: tree to go through
- * Return: the height
+ * heap_insert - heap inserter
+ *
+ * @tree: double pointer to root
+ * @value: the value to store
+ * Return: pointer to the created node
  */
-
-size_t binary_tree_height(const binary_tree_t *tree)
+heap_t *heap_insert(heap_t **tree, int value)
 {
-	size_t l = 0;
-	size_t r = 0;
+	size_t size;
+	int doswap = 1;
+	heap_t *new, *parent;
 
 	if (tree == NULL)
+		return (NULL);
+
+	if (*tree == NULL)
 	{
-		return (0);
+		new = binary_tree_node(NULL, value);
+		*tree = new;
+		return (new);
 	}
+
+	size = bts(*tree);
+	parent = heap_nf(*tree, (size - 1) / 2);
+	new = binary_tree_node(parent, value);
+	if (size % 2 == 1)
+		parent->left = new;
 	else
+		parent->right = new;
+	if (new == NULL)
+		return (NULL);
+
+	while (doswap == 1 && parent != NULL)
 	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (tree->parent != NULL);
-		if (tree)
+		if (parent->n >= new->n)
+			doswap = 0;
+		else
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			new = heap_r(new, parent);
+			parent = new->parent;
 		}
-		return ((l > r) ? l : r);
-		}
+	}
+
+	return (new);
 }
 
 /**
- * binary_tree_balance - Measures balance factor of a binary tree
- * @tree: tree to go through
- * Return: balanced factor
+ * bts - measure the tree
+ *
+ * @tree: pointer to root
+ * Return: size, otherwise 0
  */
-int binary_tree_balance(const binary_tree_t *tree)
+
+size_t bts(const binary_tree_t *tree)
 {
-	int right = 0, left = 0, total = 0;
-
-	if (tree)
-	{
-		left = ((int)binary_tree_height(tree->left));
-		right = ((int)binary_tree_height(tree->right));
-		total = left - right;
-	}
-	return (total);
-}
-
-/**
- * tree_is_perfect - function that says if a tree is perfect or not
- * it has to be the same quantity of levels in left as right, and also
- * each node has to have 2 nodes or none
- * @tree: tree to check
- * Return: 0 if is not a perfect or other number that is the level of height
- */
-int tree_is_perfect(const binary_tree_t *tree)
-{
-	int l = 0, r = 0;
-
-	if (tree->left && tree->right)
-	{
-		l = 1 + tree_is_perfect(tree->left);
-		r = 1 + tree_is_perfect(tree->right);
-		if (r == l && r != 0 && l != 0)
-			return (r);
-		return (0);
-	}
-	else if (!tree->left && !tree->right)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-/**
- * binary_tree_is_perfect - perfect or not a tree
- * @tree: tree to check
- * Return: 1 is it is or 0 if not
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int result = 0;
-
 	if (tree == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		result = tree_is_perfect(tree);
-		if (result != 0)
-		{
-			return (1);
-		}
-		return (0);
-	}
+	return (1 + bts(tree->left) + bts(tree->right));
 }
 
 /**
- * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
- * @tree: tree to check
- * Return: 1 is it is or 0 if not
+ * heap_nf - heap node finder
+ *
+ * @root: pointer to the root
+ * @number: index of the node to find
+ * Return: pointer to the node
  */
-int binary_tree_is_heap(const binary_tree_t *tree)
+heap_t *heap_nf(heap_t *root, size_t number)
 {
-	int bval;
+	size_t parentn, dir;
 
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	if (tree->left && tree->left->n > tree->n)
-	{
-		return (0);
-	}
-	if (tree->right && tree->right->n > tree->n)
-	{
-		return (0);
-	}
-	if (binary_tree_is_perfect(tree))
-	{
-		return (1);
-	}
-	bval = binary_tree_balance(tree);
-	if (bval == 0)
-	{
-		return (binary_tree_is_perfect(tree->left)
-			&& binary_tree_is_heap(tree->right));
-	}
-	if (bval == 1)
-	{
-		return (binary_tree_is_heap(tree->left)
-			&& binary_tree_is_perfect(tree->right));
-	}
-	else
-	{
-		return (0);
-	}
+	if (number == 0)
+		return (root);
+
+	parentn = (number - 1) / 2;
+	dir = (number - 1) % 2;
+
+	if (dir == 0)
+		return (heap_nf(root, parentn)->left);
+	return (heap_nf(root, parentn)->right);
+}
+
+/**
+ * heap_r - heap rebalancer
+ * @new: pointer to the new node
+ * @parent: pointer to the new parent
+ * Return: parent
+ */
+heap_t *heap_r(heap_t *new, heap_t *parent)
+{
+	int tmp;
+
+	tmp = new->n;
+	new->n = parent->n;
+	parent->n = tmp;
+	return (parent);
 }
